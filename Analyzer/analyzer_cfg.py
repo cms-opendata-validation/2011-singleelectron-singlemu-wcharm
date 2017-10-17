6e30e6e24ad5a08aebd347585fb0b82ce913595d
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2017-10-17 14:20:30 +0200

    setup Analyzer

--- Analyzer/analyzer_cfg.py
+++ Analyzer/analyzer_cfg.py
@@ -5,94 +5,125 @@ import FWCore.PythonUtilities.LumiList as LumiList
 import FWCore.Utilities.FileUtils as FileUtils
 import sys
 
-# general stuff
-process = cms.Process("Demo")
-process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.Geometry.GeometryDB_cff")
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.cerr.suppressWarning = cms.untracked.vstring('DemoAnalyzer')
-process.MessageLogger.categories.append('demo')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
-# set max events number
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50000))
-
-# ******* is it MC? *********
-gen = 0
-mc  = 1
-if gen == 1 and mc == 0: 
+########################################################################
+#################### Passed arguments ##################################
+########################################################################
+#
+# can be invoked with no parameters passed, in this case use default values
+#
+# input file name
+inFileTest = 'root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/W1Jet_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S13_START53_LV6-v1/00000/003B2BE1-4514-E411-9FA4-00261894395F.root'
+# for fast tests, you can copy input ROOT files to the local machine
+#inFileTest = 'file:/home/cms-opendata/cmsOpenDataFiles/003B2BE1-4514-E411-9FA4-00261894395F.root'
+#
+# output file name
+outFileTest = 'wcharmTmp.root'
+#
+# flags which determine what will be done
+flag_reco = 1   # process reconstruction level
+flag_gen  = 1   # process generated level
+flag_mc   = 1   # 1 for mc, 0 for data
+#
+# process passed arguments, if any
+#
+if len(sys.argv) < 4:
+  print("Usage: cmsRun analyzer_cfg.py <input list> <output file> <reco flag> <gen flag> <mc flag>")
+  inputList = inFileTest
+  outFile = outFileTest
+  # do not stop execution at this point, run with default arguments
+  #sys.exit("Wrong usage!")
+else:                 
+  inputList = FileUtils.loadListFromFile(sys.argv[2])
+  outFile   = sys.argv[3]
+  flag_reco = int(sys.argv[4])
+  flag_gen  = int(sys.argv[5])
+  flag_mc   = int(sys.argv[6])
+# consistency check
+if flag_gen == 1 and flag_mc == 0: 
   sys.exit("Error: gen = 1 requires mc = 1")
-# ************************************************
-
-# input and output
-#inFileTest = 'file:root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/W1Jet_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S13_START53_LV6-v1/30000/0472896B-D713-E411-8C82-0025905A60D2.root'
-inFileTest = 'file:/home/cms-opendata/cmsOpenDataFiles/00FB9565-01B4-E311-9233-00248C0BE014.root'
-outFileTest = 'wcharmSelTmp.root'
-#cacheSize = cms.untracked.uint32(100*1024*1024),
-if len(sys.argv) < 4: print("Usage: cmsRun analyzer_cfg.py <input list> <output file>");inputList = inFileTest;outFile = outFileTest#; sys.exit("Wrong usage!")
-else:                 inputList = FileUtils.loadListFromFile(sys.argv[2]);outFile = sys.argv[3]
-if len(sys.argv) < 4: process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(inputList))
-else: process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(*inputList),
-)
-# ************************************************
-
-# global tag from http://opendata.cern.ch/getting-started/CMS?ln=en
+#
+########################################################################
+#
+########################################################################
+#################### Prepare and run Analyzer ##########################
+########################################################################
+#
+process = cms.Process("Demo")
+# load needed tools
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+#
+# global tag as described at http://opendata.cern.ch/getting-started/CMS?ln=en
+if flag_mc == 0:
+# DATA
 # Before should be done:
 #    ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA FT_53_LV5_AN1
 #    ln -sf /cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db FT_53_LV5_AN1_RUNA.db
-if mc == 0:
-  # global tag data
   process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db')
   process.GlobalTag.globaltag = 'FT_53_LV5_AN1::All'
+# MC
+# Before should be done:
+#ln -sf /cvmfs/cms-opendata-conddb.cern.ch/START53_LV6A1.db START53_LV6A1.db
+#ln -sf /cvmfs/cms-opendata-conddb.cern.ch/START53_LV6A1 START53_LV6A1
 else:
-  # global tag MC
   process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/START53_LV6A1.db')
   process.GlobalTag.globaltag = 'START53_LV6A1::All'
-# ************************************************
-
-# apply JSON
-if mc == 0:
+#
+# intialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'ERROR'
+process.MessageLogger.categories.append('Demo')
+# change the value below if you want more or less status output
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit = cms.untracked.int32(-1))
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+#
+# Change this to a positive value to limit the number of processed events
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#
+# supply processor with input files
+if len(sys.argv) < 4:
+  # only one file in the list
+  process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(inputList))
+else:
+  # many files in the list
+  process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(*inputList))
+#
+# JSON (good luminosity sections), only if processing data
+if flag_mc == 0:
   goodJSON = 'data/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt'
-  myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
-  process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  process.source.lumisToProcess.extend(myLumis)
-# ************************************************
-
+  myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',') 
+  process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
+  process.source.lumisToProcess.extend(myLumis) 
+#
 # Load jet correction services for all jet algoritms
 process.load("JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff")
-
-# run analyzer
-process.demo = cms.EDAnalyzer('DemoAnalyzer',outFile = cms.string(outFile), gen = cms.int32(gen))
+#
+# all is ready: pass all arguments to Analyzer (C++ code in src/Analyzer.cc)
+process.demo = cms.EDAnalyzer('Analyzer', outFile = cms.string(outFile), mc = CfgTypes.int32(flag_mc), reco = CfgTypes.int32(flag_reco), gen = CfgTypes.int32(flag_gen))
 process.p = cms.Path(process.demo)
-# ************************************************
-
-# ************************************************
-# ************************************************
-# ************************************************
-# print MC generated particles (only hard scattering)
-#process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-#process.printDecay = cms.EDAnalyzer("ParticleDecayDrawer",
-#    src = cms.InputTag("genParticles"),
-#    printP4 = cms.untracked.bool(True),
-#    printPtEtaPhi = cms.untracked.bool(False),
-#    printVertex = cms.untracked.bool(False)
-#  )
-#process.p = cms.Path( process.printDecay)
-#  
-# print MC generated particles
+#
+########################################################################
+#
+########################################################################
+#################### ParticleTreeDrawer ################################
+########################################################################
+#
+# this is another processor which prints MC generated particles
+# (if you wanr this, disable Analyzer above)
+#
 #process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 #process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
 #                                   src = cms.InputTag("genParticles"),                                                                 
-#                                   printP4 = cms.untracked.bool(True),
-#                                   printPtEtaPhi = cms.untracked.bool(False),
+#                                   printP4 = cms.untracked.bool(False),
+#                                   printPtEtaPhi = cms.untracked.bool(True),
 #                                   printVertex = cms.untracked.bool(False),
 #                                   printStatus = cms.untracked.bool(True),
 #                                   printIndex = cms.untracked.bool(False),
-#                                   status = cms.untracked.vint32( 3 )
+#                                   #status = cms.untracked.vint32( 3 )
 #                                   )
-#process.p = cms.Path(process.printDecay * process.printTree)
-# ************************************************
+#process.p = cms.Path( process.printTree)
+#
+########################################################################
---
+++ Analyzer/run.sh
@@ -0,0 +1,126 @@
+#/bin/bash
+#
+# This is the main script to run cmsRun jobs, both for data and MC
+# First configuration part comes (set 'INPUTLIST', 'OUTPUTDIR', 
+# 'reco', 'gen', 'mc', 'NP', 'outrootsuffix'), then call to cmsRun, 
+# which process analyzer_cfg.py file (find there further description 
+# if needed). To run this script, execute the command:
+# ./run.sh. You need to run this script once per each data and MC sample 
+# (see below INPUTLIST variable).
+#
+# You may run this script in the terminal and close the terminal, 
+# the jobs will keep working (nohup). If you need to kill all running 
+# jobs, execute the command 'killall -9 cmsRun'. To monitor running 
+# jobs you could use './running.sh <dir>' and './processed.sh <dir>', 
+# where <dir> is the output directory.
+#
+########################################################################
+########################## Input lists #################################
+########################################################################
+#
+# Uncomment one 'INPUTLIST' and the corresponding 'OUTPUTDIR'
+# (by default data SingleMu sample is uncommented). 
+# For Monte Carlo (MC) set mc = 1 below, for signal MC also set gen = 1
+#
+INPUTLIST='data/CMS_Run2011A_SingleMu_AOD_12Oct2013-v1-all_file_index.txt'
+#INPUTLIST='data/CMS_Run2011A_SingleElectron_AOD_12Oct2013-v1-all_file_index.txt'
+#
+# MC ('W + c' signal and 'W + jets other' background) - most time consuming!
+#INPUTLIST='mc/CMS_MonteCarlo2011_Summer11LegDR_W1Jet_TuneZ2_7TeV-madgraph-tauola_AODSIM_PU_S13_START53_LV6-v1-all_file_index.txt'
+#
+########################################################################
+
+########################################################################
+########################## Output directory ############################
+########################################################################
+#
+# Uncomment one 'OUTPUTDIR' based on the the corresponding 'INPUTLIST'
+# (this is just a name: it could be any, but makes sence to use consistent one)
+# (by default the directory for data SingleMu sample is uncommented). 
+# The produced output ROOT files in this directory have to be 
+# processed by PostAnalyzer after all (see PostAnalyzer/README.txt).
+#
+# Data
+OUTPUTDIR='ntuples-data/_SingleMu'
+#OUTPUTDIR='ntuples-data/_SingleElectron'
+#
+# MC ('W + c' signal and 'W + jets other' background)
+#OUTPUTDIR='ntuples-mc/W1Jet_TuneZ2_7TeV-madgraph-tauola'
+#
+########################################################################
+
+########################################################################
+########################## Flags #######################################
+########################################################################
+#
+# Process reconstruction level (normally always should be done, 
+# unless you want some pure generator level MC study)
+reco=1 
+# Process generator level (should be done for signal MC to store 
+# "true" information for detector efficiency corrections,
+# not needed for background MC)
+gen=0
+# For MC set to 1
+mc=0
+########################################################################
+
+########################################################################
+#################### Parallel jobs settings ############################
+########################################################################
+#
+# Very important part! Proper setting here might make your analysis ~ 10 times faster.
+# It will definitely require some exercises to find optimal number jobs for your machine. 
+# The default setting NP = 1 is the most simple and will always work, 
+# although performance-wise it is most likely least optimal.
+# The basic feature is that the data server responce is latent 
+# (typically data are read in portions of a few MB each 1..100 seconds), 
+# this can be overcome by running many parallel jobs.
+# Things to consider when finding the optimal number of jobs:
+#  1) each parallel job eats about 250MB..1GB memory (depending on data or MC sample, how long is running etc.)
+#  2) on Intel Core i5-5300U (2.3GHz) one processor core becomes ~100% busy with ~5 jobs
+#  3) depends heavily on the network access (with slow network you will not win much with many parallel jobs)
+#  4) timing results can be quite stochastic
+# Splitting of input files between parallel jobs is done automatically
+# (there will be NP root and log files in the output directory).
+#
+NP=1
+outrootsuffix='' # optional suffix for output root file names (can be a subdirectory, for instance)
+#
+########################################################################
+
+########################################################################
+#################### Call cmsRun analyzer_cfg.py #######################
+########################################################################
+#
+# This part just makes some checks, prepares arguments and passes them 
+# to analyzer_cfg.py (settings chosen above): normally you do not need 
+# to change anything here
+#
+if [ ! -f $INPUTLIST ]
+then
+  echo "Error: no input file $INPUTLIST"
+  exit 1
+fi
+if [ -d $OUTPUTDIR ]
+then
+  echo "Error: output directory $OUTPUTDIR exists"
+  exit 1
+else
+  mkdir -p $OUTPUTDIR
+  p=0
+  for inputFile in `cat $INPUTLIST`
+  do
+    p=$[$p+1]
+    echo $inputFile >> "$OUTPUTDIR/inputList"${outrootsuffix}"_$p.txt"
+    if [ $p == $NP ]; then p=0; fi
+  done
+fi
+# call cmsRun analyzer_cfg.py for each parallel job
+for p in `seq 1 $NP`
+do
+  command="time cmsRun analyzer_cfg.py ${OUTPUTDIR}/inputList${outrootsuffix}_${p}.txt ${OUTPUTDIR}/ttbarSel${outrootsuffix}_${p}.root ${reco} ${gen} ${mc}"
+  nohup ${command} >& ${OUTPUTDIR}/log${outrootsuffix}_${p}.txt&
+done
+########################################################################
+
+exit 0
