f8954ab947d2b688c29761e22d65e8e9227856cc
Oleksandr Zenaiev <oleksandr.zenaiev@cern.ch>
2017-10-19 01:33:31 +0200

    fixed compilation on Ubuntu

--- PostAnalyzer/compile.sh
+++ PostAnalyzer/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # compile code (produces two executables)
-g++ -o wcharmMakeHist `root-config --cflags --libs` -lMathMore -std=c++11 wcharmMakeHist.cxx
-g++ -g -o wcharmMakePlots `root-config --cflags --libs` -std=c++11 wcharmMakePlots.cxx
+g++ wcharmMakeHist.cxx -o wcharmMakeHist `root-config --cflags --libs` -lMathMore -std=c++11
+g++ wcharmMakePlots.cxx -o wcharmMakePlots `root-config --cflags --libs` -std=c++11
 
 # create needed directories if do not exist yet
 mkdir -p data mc hist plots
